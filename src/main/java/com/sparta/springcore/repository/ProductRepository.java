b496d68e5bed8876b3c4fc0e0a3fde148fe09042
h3yon <khykhy1006@gmail.com>
2021-09-10 01:01:14 +0900

    feat: select each folder's likeProduct

--- src/main/java/com/sparta/springcore/repository/ProductRepository.java
+++ src/main/java/com/sparta/springcore/repository/ProductRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUserId(Long userId, Pageable pageable);
+    // 오 자동적으로 생긴 테이블 Folder id를 찾아서 진행함.
+    Page<Product> findAllByUserIdAndFolderList_Id(Long userId, Long folderId, Pageable pageable);
 }
